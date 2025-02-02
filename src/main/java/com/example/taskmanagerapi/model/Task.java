fb5a6be9beeec2192cb87cae3cb5ef5a40c6a349
Genilson Almeida <genilsonalmeidajr@gmail.com>
2025-02-01 23:15:02 -0300

    "feat: implement task management functionality: Added endpoints for creating, updating, deleting, and fetching tasks"

--- src/main/java/com/example/taskmanagerapi/model/Task.java
+++ src/main/java/com/example/taskmanagerapi/model/Task.java
@@ -1,4 +1,23 @@
 package com.example.taskmanagerapi.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private LocalDateTime createDate = LocalDateTime.now();
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
 }
